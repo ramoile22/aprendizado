8899cd5e00703e2dc4a2162eebc8fa6e01272ecb
ramoile22 <ramoile956@gmail.com>
2026-03-29 17:59:17 -0300

    Add files via upload

---
+++ aula06.py
@@ -0,0 +1,9 @@
+n1= int(input('Digite um número'))
+n2= int(input('Digite mais um número'))
+r= n1+n2
+#print(f'A soma entre {n1} e {n2} vale {r}.')
+print("A soma vale {} e {} vale {}".format(n1, n2, r))
+n3= int(input('Digite um número: '))
+n4=int(input('Digite mais um número: '))
+r2= n3-n4
+print(f'A subtração entre {n3} e {n4} vale {r2}')
