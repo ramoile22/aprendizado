d4a9365e999c5a476f45721b73f95927b56c77b2
ramoile22 <ramoile956@gmail.com>
2026-04-02 09:45:21 -0300

    Add files via upload

---
+++ Aula07-2.py
@@ -0,0 +1,2 @@
+resposta = input('Qual e´o seu nome?')
+print('Meu nome é {:>20}.'.format(resposta))
---
+++ aula07.py
@@ -0,0 +1,16 @@
+n1 = int(input('Digite um número'))
+n2 = int(input('Digite outro número'))
+r1 = n1 + n2
+r2 = n1 - n2
+r3 = n1 * n2
+r4 = n1 / n2
+r5 = n1 ** n2
+r6 = n1 // n2
+r7 = n1 % n2
+print( 'os resultados,na ordem: adição:{},subtração:{},multiplicação{},divisão{},potência{},divisão exata{},resto da divisão{}'.format(r1,r2,r3,r4,r5,r6,r7))
+rf = r1+r2-r3+r4-r5-r6+r7
+print('o resultado com tudo junto{}'.format(rf))
+
+
+
+
