769e14e4badd798a5c8e34878c0159fa16aa2d73
Elisa Dias <ramoile956@gmail.com>
2026-06-20 20:16:28 -0300

    Add files via upload

---
+++ aula014.py
@@ -0,0 +1,22 @@
+from time import sleep
+c = 1
+n = 1
+while c < 10:
+    print(c)
+    c += 1
+'''Aqui temos um progama onde
+o while funciona como um for'''
+
+print('\033[1;33mEstabilizando-se...\033[m')
+sleep(10)
+#Passo a passo para utilizar um while:
+'''1:'''
+while n != 0:
+    '''2:'''
+    n = int(input('Digite um valor: '))
+'''3:'''
+print('Fim')
+# Tradução:
+#1: use uma variável de controle para criar um laço com WHILE
+#2: Dentro da variável, crie um input onde guarda vários valores aleatórios
+#3: Divirta-se com o programa! Quando tiver um valor igual a zero, ele vai parar
